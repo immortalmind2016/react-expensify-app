efa77d2b1f9c194379e51abf47e27841da2340da
immortal mind <immortal.mind2016@gmail.com>
2018-09-13 01:35:08 +0200

    setup test database env

--- webpack.config.js
+++ webpack.config.js
@@ -45,12 +45,12 @@ return {
   plugins:[
     CSSExtract,  //will pass this values through bundle.js
     new webpack.DefinePlugin({
-      "process.env.FIREBASE_API_KEY":JSON.stringfy(process.env.FIREBASE_API_KEY),
-      "process.env.FIREBASE_AUTH_DOMAIN":JSON.stringfy(process.env.FIREBASE_AUTH_DOMAIN),
-      "process.env.FIREBASE_DATA_BASE_URL":JSON.stringfy(process.env.FIREBASE_DATA_BASE_URL),
-      "process.env.FIREBASE_PROJECT_ID":JSON.stringfy(process.env.FIREBASE_PROJECT_ID),
-      "process.env.FIREBASE_STORAGE_BUCKET":JSON.stringfy(process.env.FIREBASE_STORAGE_BUCKET),
-      "process.env.FIREBASE_MESSAGING_SENDER_ID":JSON.stringfy(process.env.FIREBASE_MESSAGING_SENDER_ID)
+      "process.env.FIREBASE_API_KEY":JSON.stringify(process.env.FIREBASE_API_KEY),
+      "process.env.FIREBASE_AUTH_DOMAIN":JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+      "process.env.FIREBASE_DATA_BASE_URL":JSON.stringify(process.env.FIREBASE_DATA_BASE_URL),
+      "process.env.FIREBASE_PROJECT_ID":JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+      "process.env.FIREBASE_STORAGE_BUCKET":JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+      "process.env.FIREBASE_MESSAGING_SENDER_ID":JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID)
 
     })
   ],
